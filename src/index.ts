f5ae37e7d0e32ac87f124cd6e839363ab6216a42
khaoxkongz <thanapat.makkawal@gmail.com>
2024-02-20 23:09:49 +0700

    feat: create note endpoint

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,23 @@ app.get("/api/notes", async (req, res) => {
 	res.json(notes);
 });
 
+app.post("/api/notes", async (req, res) => {
+	const { title, content } = req.body;
+
+	if (!title || !content) {
+		res.status(400).send("title and content fields required");
+	}
+
+	try {
+		const note = await prisma.note.create({
+			data: { title, content },
+		});
+		res.json(note);
+	} catch (error) {
+		res.status(500).send("oops something went wrong");
+	}
+});
+
 app.listen(8888, () => {
 	console.log("server running on localhost:8888");
 });
